0fc75616cd2427be1694c776eee5330af71149a6
Dave Syer <dsyer@vmware.com>
2022-03-23 12:58:31 +0000

    No need for overlays

--- shell.nix
+++ shell.nix
@@ -1,33 +1,23 @@
-with import <nixpkgs> {
-  overlays = [
-    (self: super: {
-        nixos-playwright = super.stdenv.mkDerivation {
-        pname = "nixos-playwright";
-        version = "0.0.1";
-        src = super.fetchgit {
-          url =
-            "https://github.com/ludios/nixos-playwright";
-          sha256 = "1yb4dx67x3qxs2842hxhhlqb0knvz6ib2fmws50aid9mzaxbl0w0";
-        };
-        phases = [ "installPhase" ];
-        buildPhase = ''
-          echo "Nothing here"
-        '';
-        installPhase = ''
-          mkdir -p $out/bin
-          cd $out/bin && cp $src/* .
-        '';
-      };
-    })
-  ];
-};
-mkShell {
+with import <nixpkgs> { };
+let
+  nixos-playwright = stdenv.mkDerivation {
+    pname = "nixos-playwright";
+    version = "0.0.1";
+    src = fetchgit {
+      url = "https://github.com/ludios/nixos-playwright";
+      sha256 = "1yb4dx67x3qxs2842hxhhlqb0knvz6ib2fmws50aid9mzaxbl0w0";
+      rev = "fdafd9d4e0e76bac9283c35a81c7c0481a8b1313";
+    };
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cd $out/bin && cp $src/* .
+    '';
+  };
+in mkShell {
 
   name = "env";
-  buildInputs = [
-    figlet
-    nixos-playwright
-  ];
+  buildInputs = [ figlet nixos-playwright ];
 
   shellHook = ''
     export MESSAGE=Hello
